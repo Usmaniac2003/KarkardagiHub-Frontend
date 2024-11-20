d9c072e5030ed65de8e3904d744f815a0bd2bf58
Usman <usmancodepro@gmail.com>
2024-11-20 06:49:13 +0500

    Initial Setup

---
+++ README.md
@@ -0,0 +1,5 @@
+Open Terminal
+Press npm install
+Press npm run dev
+Then Ctrl+Click on the given terminal to run your app,
+
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-500">Karkardagi Hub</h1>
+    </div>
+  );
+}
