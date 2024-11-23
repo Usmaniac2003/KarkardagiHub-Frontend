332a1b8484060d964475ddc93747be03eb564c6a
Usman <usmancodepro@gmail.com>
2024-11-24 03:34:46 +0500

    Auth Updated

--- src/pages/General/Home.jsx
+++ src/pages/General/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
 
   const logOut = () => {
     logout();
-    navigate("/login");
+    navigate("/");
   };
 
   return (
--- src/pages/General/NoPanel.jsx
+++ src/pages/General/NoPanel.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useAuth } from '../../context/Auth';
 
 function NoPanel() {
+  const { user, logout, loading } = useAuth();
+  const logOut = () => {
+    logout();
+    navigate("/");
+  };
   return (
     <div>
       You are yet to be assigned a role.
+      <button
+        onClick={logOut}
+        className="text-2xl text-red-300 font-extrabold p-6 border-black"
+      >
+        Logout
+      </button>
     </div>
   )
 }
